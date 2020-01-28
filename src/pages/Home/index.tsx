a447d98247f699f886756e202dcf19b3eb5855a2
Ryan The <ryan.the.2006@gmail.com>
2020-01-28 22:12:35 +0800

    Add category cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,9 @@ import {
   SafeAreaView,
   StatusBar,
   Platform,
-  ScrollView, TouchableOpacity, Image,
+  ScrollView,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
 import {HeaderSmall} from '../../components/Text/HeaderSmall';
 import {Header} from '../../components/Text/Header';
@@ -14,18 +16,21 @@ import Confetti from 'react-native-confetti';
 import pvpTalk from '../../../assets/images/pvp_talk.png';
 import {SpecialEventCard} from '../../components/Card/SpecialEvent';
 import {NavigationInjectedProps} from 'react-navigation';
-import {EventCard} from "../../components/Card/Event";
+import {EventCard} from '../../components/Card/Event';
+import {EventCatCard} from '../../components/Card/EventCategory';
+
 // @ts-ignore
-import anniversaryConfetti from '../../../assets/images/anniversary_confetti.png'
+import anniversaryConfetti from '../../../assets/images/anniversary_confetti.png';
 // @ts-ignore
-import {Layout, Icon, withStyles, Text} from "@ui-kitten/components";
-import {ThemedIcon} from "../../components/Icon/ThemedIcon";
-import {ThemeContext} from "../../functions/theme";
+import {Layout, Icon, withStyles, Text} from '@ui-kitten/components';
+import {ThemedIcon} from '../../components/Icon/ThemedIcon';
+import {ThemeContext} from '../../functions/theme';
 // @ts-ignore
-import gettingToSST from '../../../assets/images/getting_to_sst.png'
+import gettingToSST from '../../../assets/images/getting_to_sst.png';
+import { FlatList } from 'react-native-gesture-handler';
 
 class HomePage extends React.Component<NavigationInjectedProps> {
-  static contextType = ThemeContext
+  static contextType = ThemeContext;
   static navigationOptions = {
     header: null,
   };
@@ -37,7 +42,7 @@ class HomePage extends React.Component<NavigationInjectedProps> {
         style={{
           flex: 1,
         }}>
-        <StatusBar barStyle={'dark-content'} backgroundColor={'white'}/>
+        <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
         <SafeAreaView
           style={{
             flex: 1,
@@ -48,7 +53,7 @@ class HomePage extends React.Component<NavigationInjectedProps> {
               paddingLeft: 25,
               paddingRight: 25,
               flexDirection: 'row',
-              alignItems: 'center'
+              alignItems: 'center',
             }}>
             <View>
               <HeaderSmall
@@ -82,35 +87,42 @@ class HomePage extends React.Component<NavigationInjectedProps> {
                 Events
               </Header>
             </View>
-            <View style={{
-              flex: 1,
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              flexDirection: 'row'
-            }}>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                flexDirection: 'row',
+              }}>
               <ThemeContext.Consumer>
-                {theme => <TouchableOpacity style={{marginRight: 15}} onPress={() => {
-                  theme.toggleTheme()
-                }}>
-                  <ThemedIcon name={"search"} size={30}/>
-                </TouchableOpacity>}
+                {theme => (
+                  <TouchableOpacity
+                    style={{marginRight: 15}}
+                    onPress={() => {
+                      theme.toggleTheme();
+                    }}>
+                    <ThemedIcon name={'search'} size={30} />
+                  </TouchableOpacity>
+                )}
               </ThemeContext.Consumer>
-              <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
-                <Layout style={{
-                  padding: 10,
-                  borderRadius: 25,
-                  elevation: 10,
-                  shadowColor: 'black',
-                  shadowOffset: {width: 0, height: 0},
-                  shadowOpacity: 0.16,
-                  shadowRadius: 10,
-                  width: 50,
-                  height: 50,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  marginLeft: 20
-                }}>
-                  <ThemedIcon name={'menu'} size={30}/>
+              <TouchableOpacity
+                onPress={() => this.props.navigation.openDrawer()}>
+                <Layout
+                  style={{
+                    padding: 10,
+                    borderRadius: 25,
+                    elevation: 10,
+                    shadowColor: 'black',
+                    shadowOffset: {width: 0, height: 0},
+                    shadowOpacity: 0.16,
+                    shadowRadius: 10,
+                    width: 50,
+                    height: 50,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginLeft: 20,
+                  }}>
+                  <ThemedIcon name={'menu'} size={30} />
                 </Layout>
               </TouchableOpacity>
             </View>
@@ -120,7 +132,7 @@ class HomePage extends React.Component<NavigationInjectedProps> {
               paddingBottom: 25,
               paddingRight: 25,
               paddingLeft: 25,
-              paddingTop: 25
+              paddingTop: 25,
             }}>
             <SpecialEventCard
               image={pvpTalk}
@@ -129,49 +141,61 @@ class HomePage extends React.Component<NavigationInjectedProps> {
               time={`10.30am to\n11.30am`}
             />
             <ThemeContext.Consumer>
-              {theme => <EventCard
-                style={{
-                  backgroundColor: theme.theme === 'light' ? 'rgba(18, 113, 237, 0.24)' : '#0D57CB'
-                }}
-                onPress={() => {
-                  this.props.navigation.navigate("Anniversary")
-                }}
-              >
-                <View style={{
-                  marginLeft: 25,
-                  marginRight: 25,
-                  flex: 1,
-                  alignItems: 'center',
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}>
-                  <Text category={'h5'} style={{
-                    opacity: 0.75,
-                    ...Platform.select({
-                      android: {
-                        fontFamily: 'Raleway 700'
-                      },
-                      ios: {
-                        fontFamily: 'Raleway',
-                        fontWeight: '700'
-                      }
-                    }),
-                    lineHeight: 25,
-                    marginTop: 20,
-                    marginBottom: 20,
-                  }}>{`Getting to\nSST`}
-                  </Text>
-                  <View style={{
-                    marginTop: 5,
-                    marginBottom: 5,
+              {theme => (
+                <EventCard
+                  style={{
+                    backgroundColor:
+                      theme.theme === 'light'
+                        ? 'rgba(18, 113, 237, 0.24)'
+                        : '#0D57CB',
+                  }}
+                  onPress={() => {
+                    this.props.navigation.navigate('Anniversary');
                   }}>
-                    <Image source={gettingToSST} style={{
-                      resizeMode: 'contain',
-                      height: '100%',
-                    }}/>
+                  <View
+                    style={{
+                      marginLeft: 25,
+                      marginRight: 25,
+                      flex: 1,
+                      alignItems: 'center',
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                    }}>
+                    <Text
+                      category={'h5'}
+                      style={{
+                        opacity: 0.75,
+                        ...Platform.select({
+                          android: {
+                            fontFamily: 'Raleway 700',
+                          },
+                          ios: {
+                            fontFamily: 'Raleway',
+                            fontWeight: '700',
+                          },
+                        }),
+                        lineHeight: 25,
+                        marginTop: 20,
+                        marginBottom: 20,
+                      }}>
+                      {`Getting to\nSST`}
+                    </Text>
+                    <View
+                      style={{
+                        marginTop: 5,
+                        marginBottom: 5,
+                      }}>
+                      <Image
+                        source={gettingToSST}
+                        style={{
+                          resizeMode: 'contain',
+                          height: '100%',
+                        }}
+                      />
+                    </View>
                   </View>
-                </View>
-              </EventCard>}
+                </EventCard>
+              )}
             </ThemeContext.Consumer>
             <EventCard
               image={anniversaryConfetti}
@@ -193,10 +217,37 @@ class HomePage extends React.Component<NavigationInjectedProps> {
                     fontFamily: 'Raleway 700',
                   },
                 }),
+                //app builds tho hmmm..........
                 marginTop: 20,
               }}>
               Categories
             </Header>
+            <View
+            style={{
+              flexDirection: 'row'
+              }}>
+              <EventCatCard
+                title="Essential"
+                icon="book-outline"
+                onPress={() => {
+                  this.props.navigation.navigate('Anniversary');
+                }}
+              />
+              <EventCatCard
+                title="Hands On"
+                icon="book-outline"
+                onPress={() => {
+                  this.props.navigation.navigate('Anniversary');
+                }}
+              />
+              <EventCatCard
+                title="Hands On"
+                icon="book-outline"
+                onPress={() => {
+                  this.props.navigation.navigate('Anniversary');
+                }}
+              />
+            </View>
           </ScrollView>
         </SafeAreaView>
         <Confetti
@@ -209,4 +260,4 @@ class HomePage extends React.Component<NavigationInjectedProps> {
   }
 }
 
-export default HomePage
+export default HomePage;
