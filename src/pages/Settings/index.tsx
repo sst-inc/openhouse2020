251c4dd2319d0bcbc6096fe8f5cc37331d2be933
Ryan The <ryan.the.2006@gmail.com>
2020-01-31 12:49:08 +0800

    Update Settings with PageHeader

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -4,30 +4,14 @@ import {Platform, SafeAreaView, View} from "react-native";
 import {HeaderSmall} from "../../components/Text/HeaderSmall";
 import {Header} from "../../components/Text/Header";
 import {userLogOut} from "../../functions/user";
+import {PageHeader} from '../../components/PageHeader';
 
 class SettingsPage extends React.Component {
   render() {
     return (
       <Layout style={{flex: 1}}>
         <SafeAreaView style={{flex: 1}}>
-          <View style={{paddingLeft: 25, paddingRight: 25, paddingTop: 10}}>
-            <Header
-              style={{
-                ...Platform.select({
-                  ios: {
-                    fontWeight: '700',
-                    fontFamily: 'Raleway',
-                  },
-                  android: {
-                    fontWeight: undefined,
-                    fontFamily: 'Raleway 700',
-                  },
-                }),
-                marginTop: 5,
-              }}>
-              Settings
-            </Header>
-          </View>
+          <PageHeader {...this.props} title="Settings" subtitle="" searchTrue={false} />
           <Button onPress={() => {
             userLogOut()
           }}>
