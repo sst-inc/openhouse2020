876088a961f2b62c857571c91e6648849ff58253
qinguan <qinguan20040914@gmail.com>
2020-02-01 00:50:10 +0800

    Revert "Revert "Merge remote-tracking branch 'github-desktop-theboi/master' into pr/4""
    This reverts commit 243a3e81a144da5a7d703bedbc6ed7163943fe63.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -151,7 +151,7 @@ class HomePage extends React.Component<NavigationInjectedProps> {
               return (
                 <CategoryCard title={item.title} icon={'facebook'}
                               onPress={() => {
-                                this.props.navigation.navigate("CategoryEvents")
+                                this.props.navigation.navigate("CategoryEvents", {selectedCategory: item.title})
                               }}/>
               )
             }}/>
