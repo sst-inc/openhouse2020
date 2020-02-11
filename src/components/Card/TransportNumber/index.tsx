e82fe6039551dc643b257a0f272e0408fac6f349
Ryan The <ryan.the.2006@gmail.com>
2020-02-11 14:53:21 +0800

    Work on Travel page

--- src/components/Card/TransportNumber/index.tsx
+++ src/components/Card/TransportNumber/index.tsx
@@ -3,14 +3,16 @@ import { Platform } from 'react-native'
 import { Layout, Text } from '@ui-kitten/components'
 
 
-export const TransportNumberCard = (props: {color?: string}) => {
+export const TransportNumberCard = (props: {color?: string, width?: number, number: string}) => {
   return (
     <Layout style={{
       backgroundColor: props.color ?? "#64C361",
-      width: 50,
+      width: props.width ?? 50,
       height: 40,
       borderRadius: 9,
-      justifyContent: 'center'
+      justifyContent: 'center',
+      marginRight: 5,
+      marginBottom: 5
       }}>
       <Text style={{
         color: 'white',
@@ -26,7 +28,7 @@ export const TransportNumberCard = (props: {color?: string}) => {
             fontFamily: 'Raleway 700'
           },
         }),
-        }}>17</Text>
+      }}>{props.number}</Text>
     </Layout>
   )
 }
--- src/pages/Travel/index.tsx
+++ src/pages/Travel/index.tsx
@@ -1,82 +1,173 @@
 import React from 'react';
 import {NavigationInjectedProps} from 'react-navigation';
-import {SafeAreaView, View, Dimensions, Platform} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  ScrollView,
+  Dimensions,
+  Platform,
+} from 'react-native';
 import {Layout, Text, styled} from '@ui-kitten/components';
 import {Header} from '../../components/Text/Header';
 import {TransportNumberCard} from '../../components/Card/TransportNumber';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+import {processTheme, ThemeContext} from '../../functions/theme';
 
 import Image from 'react-native-scalable-image';
 
 import PageHeader from '../../components/PageHeader';
 
 import sstmap from '../../../assets/images/sst_map.png';
+import {Consumer} from 'react-native-paper/lib/typescript/src/core/settings';
 
 class TravelPage extends React.Component<NavigationInjectedProps> {
   static navigationOptions = {
     header: null,
   };
-  render() {
-    return (
-      <Layout
-        style={{
-          flex: 1,
-        }}>
-        <SafeAreaView
-          style={{
-            flex: 1,
-          }}>
-          <View
-            style={{
-              paddingHorizontal: 25,
-            }}>
-            <PageHeader
-              navOption={'back'}
-              subtitle={'commute'}
-              title={'Getting to SST'}
-              navigation={this.props.navigation}
-            />
-          </View>
-          <Image
-              width={Dimensions.get('window').width}
-              source={sstmap}
-              style={{marginTop: 10}}
-            />
-          <View style={{marginHorizontal: 20}}>
-            <Header style={{fontSize: 20, marginTop: 10}}>Parking@SST</Header>
-            <Text>
-              <Text style={{
-                ...Platform.select({
-                  ios: {
-                    fontWeight: '700',
-                    fontFamily: 'Raleway'
-                  },
-                  android: {
-                    fontWeight: undefined,
-                    fontFamily: 'Raleway 700'
-                  },
-                }),
-              }}>You are strongly encouraged <Text style={{
-                textDecorationLine: 'underline',
-                ...Platform.select({
-                  ios: {
-                    fontWeight: '700',
-                    fontFamily: 'Raleway'
-                  },
-                  android: {
-                    fontWeight: undefined,
-                    fontFamily: 'Raleway 700'
-                  },
-                }),
-                }}>not</Text> to park at SST. </Text>Please park in
-              surrounding HDB estates and enter the school via Gate B or Gate D.
-            </Text>
-            <Header style={{fontSize: 20, marginTop: 10}}>MRT</Header>
 
-            <Header style={{fontSize: 20, marginTop: 10}}>Bus</Header>
-                <TransportNumberCard/>
-          </View>
-        </SafeAreaView>
-      </Layout>
+  render() {
+    const sstBuses = ['14', '74', '74E', '105', '106', '147', '166', '185'];
+    return (
+      <ThemeContext.Consumer>
+        {theme => (
+          <Layout
+            style={{
+              flex: 1,
+            }}>
+            <SafeAreaView
+              style={{
+                flex: 1,
+              }}>
+              <View
+                style={{
+                  paddingHorizontal: 25,
+                }}>
+                <PageHeader
+                  navOption={'back'}
+                  subtitle={'commute'}
+                  title={'Getting to SST'}
+                  navigation={this.props.navigation}
+                />
+              </View>
+              <ScrollView>
+                <Image
+                  width={Dimensions.get('window').width}
+                  source={sstmap}
+                  style={{marginTop: 10}}
+                />
+                <View style={{marginHorizontal: 20}}>
+                  <View style={{flexDirection: 'row', marginTop: 10}}>
+                    <Icon
+                      name="map-marker-alt"
+                      size={25}
+                      color={theme.theme === 'light' ? 'black' : 'white'}
+                      style={{marginTop: 12, marginRight: 10}}
+                    />
+                    <Header style={{fontSize: 20, textAlignVertical: 'center'}}>
+                      Location
+                    </Header>
+                  </View>
+                  <Text>1 Technology Drive, Singapore 138572</Text>
+
+                  <View style={{flexDirection: 'row', marginTop: 10}}>
+                    <Icon
+                      name="car"
+                      size={25}
+                      color={theme.theme === 'light' ? 'black' : 'white'}
+                      style={{marginTop: 12, marginRight: 10}}
+                    />
+                    <Header style={{fontSize: 20}}>Parking@SST</Header>
+                  </View>
+                  <Text>
+                    <Text
+                      style={{
+                        ...Platform.select({
+                          ios: {
+                            fontWeight: '700',
+                            fontFamily: 'Raleway',
+                          },
+                          android: {
+                            fontWeight: undefined,
+                            fontFamily: 'Raleway 700',
+                          },
+                        }),
+                      }}>
+                      You are strongly encouraged{' '}
+                      <Text
+                        style={{
+                          textDecorationLine: 'underline',
+                          ...Platform.select({
+                            ios: {
+                              fontWeight: '700',
+                              fontFamily: 'Raleway',
+                            },
+                            android: {
+                              fontWeight: undefined,
+                              fontFamily: 'Raleway 700',
+                            },
+                          }),
+                        }}>
+                        not
+                      </Text>{' '}
+                      to park at SST.{' '}
+                    </Text>
+                    Please park in surrounding HDB estates and enter the school
+                    via Gate B or Gate D.
+                  </Text>
+                  <View style={{flexDirection: 'row', marginTop: 10}}>
+                    <Icon
+                      name="subway"
+                      size={25}
+                      color={theme.theme === 'light' ? 'black' : 'white'}
+                      style={{marginTop: 12, marginRight: 10}}
+                    />
+                    <Header style={{fontSize: 20}}>MRT</Header>
+                  </View>
+                  <View style={{flexDirection: 'row'}}>
+                    <TransportNumberCard
+                      number="EW22"
+                      width={70}
+                      color="#429346"
+                    />
+                    <View style={{marginLeft: 5}}>
+                      <Text style={{
+                        ...Platform.select({
+                          ios: {
+                            fontWeight: '700',
+                            fontFamily: 'Raleway'
+                          },
+                          android: {
+                            fontWeight: undefined,
+                            fontFamily: 'Raleway 700'
+                          },
+                        }),
+                      }}>Dover MRT Station</Text>
+                      <Text>East-West Line</Text>
+                    </View>
+                  </View>
+                  <View style={{flexDirection: 'row', marginTop: 10}}>
+                    <Icon
+                      name="bus"
+                      size={25}
+                      color={theme.theme === 'light' ? 'black' : 'white'}
+                      style={{marginTop: 12, marginRight: 10}}
+                    />
+                    <Header style={{fontSize: 20}}>Bus</Header>
+                  </View>
+                  <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+                    {sstBuses.map((content, index) => {
+                      return (
+                        <TransportNumberCard number={content} key={index} />
+                      );
+                    })}
+                  </View>
+                </View>
+              </ScrollView>
+            </SafeAreaView>
+          </Layout>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
